e2e8cfb753a775c1f7dd1ef73b652ce132cf2eb5
Ron Huang <ron.huang@hp.com>
2023-08-29 18:05:44 +0800

    Add config for wezterm

---
+++ dot_wezterm.lua
@@ -0,0 +1,50 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+-- This is where you actually apply your config choices
+
+config.color_scheme = 'Darcula (base16)'
+
+config.font = wezterm.font 'FantasqueSansMono NF'
+config.font_size = 13.0
+
+config.window_decorations = "RESIZE"
+
+dev_shell = {
+  'pwsh.exe',
+  '-NoExit',
+  '-Command',
+  '&{Import-Module "C:\\Program Files\\Microsoft Visual Studio\\2022\\Enterprise\\Common7\\Tools\\Microsoft.VisualStudio.DevShell.dll"; Enter-VsDevShell 805f0455 -SkipAutomaticLocation -DevCmdArguments "-arch=x64 -host_arch=x64"}'
+}
+pwsh = {
+  'C:\\Program Files\\PowerShell\\7\\pwsh.exe'
+}
+
+config.default_prog = pwsh
+
+config.launch_menu = {
+  {
+    label = 'Developer PWSH for VS 2022',
+    args = dev_shell,
+  },
+  {
+    label = 'PowerShell',
+    args = pwsh,
+  },
+  {
+    label = 'Command Prompt',
+    args = { 'C:\\Windows\\System32\\cmd.exe' },
+  },
+}
+
+-- and finally, return the configuration to wezterm
+return config
