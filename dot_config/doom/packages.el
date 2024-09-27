e37f2ee382ecad04b4ebd9d3c78d7dacab408b56
Ron Huang <ron@hng.tw>
2024-09-27 22:07:03 +0800

    Use catppuccin theme for emacs

--- dot_config/doom/packages.el
+++ dot_config/doom/packages.el
@@ -13,6 +13,7 @@
 (package! robot-mode)
 (package! org-modern)
 (package! valign)
+(package! catppuccin-theme)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
