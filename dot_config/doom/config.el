9b096727527bfd9525cd3411c92b3dca305d0f7e
Ron Huang <ron.huang@hp.com>
2025-04-22 21:24:03 +0800

    Adjust org-mode style

--- dot_config/doom/config.el
+++ dot_config/doom/config.el
@@ -157,13 +157,9 @@
   (dolist (unicode-block '("CJK Unified Ideographs" "CJK Symbols and Punctuation" "CJK Radicals Supplement" "CJK Compatibility Ideographs"))
     (push "Sarasa Mono TC" (cadr (assoc unicode-block unicode-fonts-block-font-mapping)))))
 
-(use-package! org-modern
-  :hook (org-mode . global-org-modern-mode)
-  :config
-  (setq org-modern-table nil))
-
+;;
 (use-package! valign
-  :hook ((org-mode markdown-mode) . valign-mode)
+  :hook ((markdown-mode . valign-mode))
   :config
   (setq valign-fancy-bar t))
 
@@ -176,3 +172,52 @@
 ;; Restore evil-escape
 (after! evil-escape
   (setq evil-escape-key-sequence "jk"))
+
+;; Disable org-indent-mode
+(after! org
+  (setq org-startup-indented nil))
+
+;; Custom faces
+(after! org
+  ;; Set some faces
+  (custom-set-faces!
+    `((org-quote)
+      :foreground ,(doom-color 'blue) :extend t)
+    `((org-block-begin-line org-block-end-line)
+      :background ,(doom-color 'bg)))
+  ;; Change how LaTeX and image previews are shown
+  (setq org-highlight-latex-and-related '(native entities script)
+        org-image-actual-width (min (/ (display-pixel-width) 3) 800)))
+
+;; Use the medium weights for org-mode headings
+(after! org-mode
+  (custom-set-faces!
+    `((org-document-title)
+      :foreground ,(face-attribute 'org-document-title :foreground)
+      :height 1.3 :weight bold)
+    `((org-level-1)
+      :foreground ,(face-attribute 'outline-1 :foreground)
+      :height 1.1 :weight medium)
+    `((org-level-2)
+      :foreground ,(face-attribute 'outline-2 :foreground)
+      :weight medium)
+    `((org-level-3)
+      :foreground ,(face-attribute 'outline-3 :foreground)
+      :weight medium)
+    `((org-level-4)
+      :foreground ,(face-attribute 'outline-4 :foreground)
+      :weight medium)
+    `((org-level-5)
+      :foreground ,(face-attribute 'outline-5 :foreground)
+      :weight medium)))
+
+;; Turn off highlighting current line
+(add-hook 'text-mode-hook (lambda () (hl-line-mode -1)))
+
+;; Seemless look
+(use-package! org-appear
+  :hook
+  (org-mode . org-appear-mode)
+  :config
+  (setq org-hide-emphasis-markers t
+        org-appear-autolinks 'just-brackets))
