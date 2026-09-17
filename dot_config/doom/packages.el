eca252d274aceb04ae2a6a1dd81c59ec59545a8b
Ron Huang <ron@hng.tw>
2026-09-17 20:00:48 +0800

    Change Doom Emacs theme from Modus to Evergarden

--- dot_config/doom/packages.el
+++ dot_config/doom/packages.el
@@ -13,7 +13,6 @@
 ;; (package! some-package)
 (package! mixed-pitch)
 (package! nyan-mode)
-(package! ef-themes)
 (package! robot-mode)
 (package! valign)
 (package! ascii-table)
@@ -27,6 +26,9 @@
 ;;   :recipe (:host github :repo "username/repo"))
 (package! typst-ts-mode
   :recipe (:host codeberg :repo "meow_king/typst-ts-mode"))
+
+(package! evergarden-themes
+  :recipe (:host github :repo "ronhuang/evergarden-doomemacs"))
 
 ;; If the package you are trying to install does not contain a PACKAGENAME.el
 ;; file, or is located in a subdirectory of the repo, you'll need to specify
